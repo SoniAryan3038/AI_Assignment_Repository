90dd35acfe87abdd6c9fe0162821e44106fd9c8a
Aryan Soni <158656732+SoniAryan3038@users.noreply.github.com>
2024-07-18 15:08:03 +0530

    Add files via upload

--- SVMcode.py
+++ SVMcode.py
@@ -1,20 +1,20 @@
 # Importing necessary libraries
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
 
-# Example dataset
-# Features (X) and labels (y)
-X = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6],
-              [6, 7], [7, 8], [8, 9], [9, 10], [10, 11]])
-y = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+# Loading the iris dataset
+data = load_iris()
+X = data.data
+y = data.target
 
 # Splitting the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Creating an instance of the LogisticRegression model
-model = LogisticRegression()
+# Creating an instance of the SVC (Support Vector Classifier) model
+model = SVC(kernel='linear')  # Example with linear kernel
 
 # Training the model
 model.fit(X_train, y_train)
@@ -25,7 +25,3 @@ y_pred = model.predict(X_test)
 # Calculating the accuracy of the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy:.2f}')
-
-# Printing the coefficients
-print('Coefficients:', model.coef_)
-print('Intercept:', model.intercept_)
